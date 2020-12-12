6fba646af40829cd25904c7abbf96501dce5bee1
Michal Charemza <michal@charemza.name>
2020-12-12 18:08:30 +0000

    build: initial setup.py and CircleCI config

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='streaming-left-join',
+    version='0.0.0',
+    author='Department for International Trade',
+    author_email='webops@digital.trade.gov.uk',
+    description='Join iterables in code without loading them all in memory: similar to a SQL left join.',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/uktrade/python-streaming-left-join',
+    py_modules=[
+        'streaming_left_join',
+    ],
+    python_requires='>=3.6.3',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
